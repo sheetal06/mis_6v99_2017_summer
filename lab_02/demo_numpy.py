6533bca4c3bbab063d0b944d9dfae44f601b0ffb
sheetal06 <vxa161330@utdallas.edu>
2017-07-19 23:48:55 -0500

    Update demo_numpy.py

--- lab_02/demo_numpy.py
+++ lab_02/demo_numpy.py
@@ -1,1 +1,9 @@
 #Basics of numpy
+import numpy as np
+a = np.arange(15).reshape(3,5)
+shape = a.shape
+size = a.size
+itemsize = a.itemsize
+ndim = a.ndim
+dtype = a.dtype
+np.savetxt('demo_numpy.txt',(a,shape,size,itemsize,ndim,dtype),newline='\r\n',fmt='%s')
